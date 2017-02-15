f1673b96785eb781021a822104c543244840c5bd
Marcin Ruman <rumanmarcin@gmail.com>
2017-02-15 17:35:41 +0100

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-redux-todo-app
+Simple React-Redux TODO App
+
+--- CLI NIRVANA CHEAT SHEET ---
+
+1. npm install
+2. npm run dev / npm run prod (depending what environment: local or production)
+2. npm run dev-server // starts up the local dev server 
---
+++ src/js/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Layout extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>This is a react-redux-todo-app.</p>
+            </div>
+        );
+    }
+}
