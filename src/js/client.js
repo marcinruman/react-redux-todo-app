e6fd5c06105a73c50a5a9a52e486831741dda9a6
Marcin Ruman <rumanmarcin@gmail.com>
2017-03-05 23:50:16 +0100

    Adding store.js file

--- src/js/client.js
+++ src/js/client.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import Layout from "./Layout";
+import store from "./store";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Provider store="">
+ReactDOM.render(<Provider store={store}>
         <Layout />
     </Provider>, app);
