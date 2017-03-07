df8c87542517b941c03101482e7dadd3ff7a6fa2
Marcin Ruman <rumanmarcin@gmail.com>
2017-03-07 23:55:43 +0100

    Connecting React & Redux

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { connect } from "react-redux";
 
+@connect((store) => {
+    return {
+        foo: 1
+    }
+})
 export default class Layout extends React.Component {
     render() {
         return (
             <div>
                 <h1>Hello World!</h1>
-                <p>This is a react-redux-todo-app.</p>
+                <p>This is a react-redux-todo-app. {this.props.foo}</p>
             </div>
         );
     }
--- src/js/reducers/reducers.js
+++ src/js/reducers/reducers.js
@@ -1,5 +1,6 @@
-import { combineReducers } from "redux"
-import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from "../actions/actions"
+import { combineReducers } from "redux";
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from "../actions/actions";
+
 const { SHOW_ALL } = VisibilityFilters;
 
 function visibilityFilter(state = SHOW_ALL, action) {
