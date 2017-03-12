f7713731aed4ebb747a0c66be11d4a03af486ed7
Marcin Ruman <rumanmarcin@gmail.com>
2017-03-12 23:10:03 +0000

    Connecting React & Redux

--- src/js/client.js
+++ src/js/client.js
@@ -6,6 +6,9 @@ import Layout from "./Layout";
 import store from "./store";
 
 store.dispatch({ type: 'ADD_TODO', text: 'Read the Redux docs.', completed: true });
+store.dispatch({ type: 'ADD_TODO', text: 'Read the Redux docs.1', completed: true });
+store.dispatch({ type: 'ADD_TODO', text: 'Read the Redux docs.2', completed: false });
+store.dispatch({ type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_ALL' });
 
 const app = document.getElementById('app');
 
